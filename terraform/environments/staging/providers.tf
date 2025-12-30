e66c6e8b8a378405c23ba807bd307cdfc5aa8137
Riajul <riajul5@hotmail.co.uk>
2025-12-30 22:11:06 +0000

    Added staging environment terraform provider file

---
+++ terraform/environments/staging/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+
+    backend "s3" {
+    # Initiate S3 bucket as backend
+    bucket       = "url-shortener-riajul"
+    region       = "eu-west-2"
+    key          = "staging/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
